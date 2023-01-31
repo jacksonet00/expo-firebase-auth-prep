16515159b7a12a3ceafc8143e48c3f0dc2e6db51
Jackson Taylor <jacksoneliastaylor@gmail.com>
2023-01-31 10:37:09 -0500

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useAuthSignInWithPhoneNumber } from "@react-query-firebase/auth";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
+import { getApp, initializeApp } from 'firebase/app';
+import * as FirebaseCore from 'expo-firebase-core';
+import { QueryClient, QueryClientProvider } from "react-query";
 
-export default function App() {
+initializeApp(
+  FirebaseCore.DEFAULT_WEB_APP_OPTIONS as FirebaseCore.FirebaseOptions
+);
+
+export default function Providers() {
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
+  );
+}
+
+function App() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+
+  const recaptchaElement: React.MutableRefObject<any> = useRef(null);
+
+  const { mutate: submitPhoneNumber } = useAuthSignInWithPhoneNumber(getAuth());
+
+  function handleSubmitPhoneNumber() {
+    console.log(submitPhoneNumber({
+      phoneNumber,
+      // TODO: try appending this to the DOM in an async fn onMount
+      appVerifier: new RecaptchaVerifier('recaptcha', {}, getAuth()),
+    }));
+  }
+
+  function handleSubmitVerificationCode() {
+    console.log(verificationCode);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        value={phoneNumber}
+        onChangeText={(text) => setPhoneNumber(text)}
+        placeholder="Phone Number"
+      />
+      <Button
+        title="Submit"
+        onPress={handleSubmitPhoneNumber}
+      />
+      <TextInput
+        style={styles.input}
+        value={verificationCode}
+        onChangeText={(text) => setVerificationCode(text)}
+        placeholder="Phone Number"
+      />
+      <Button
+        title="Submit"
+        onPress={handleSubmitVerificationCode}
+      />
       <StatusBar style="auto" />
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaElement}
+        firebaseConfig={getApp().options}
+        attemptInvisibleVerification={true}
+      />
     </View>
   );
 }
@@ -17,4 +79,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  }
 });
---
+++ app.config.ts
@@ -0,0 +1,47 @@
+import { FirebaseOptions } from "expo-firebase-core";
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+const firebaseOptions: FirebaseOptions = {
+  apiKey: "AIzaSyDHqLRCKFxccWdAX8NJpHRVPPqJXAgac8w",
+  authDomain: "fir-expo-series-prep.firebaseapp.com",
+  projectId: "fir-expo-series-prep",
+  storageBucket: "fir-expo-series-prep.appspot.com",
+  messagingSenderId: "500544743241",
+  appId: "1:500544743241:web:5d424d480f14352cdf9c98"
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "expo-firebase-auth-series-prep",
+  slug: "expo-firebase-auth-series-prep",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF"
+    }
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+    config: {
+      firebase: firebaseOptions,
+    }
+  }
+});
